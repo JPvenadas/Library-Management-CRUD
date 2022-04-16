6e625f082c7f27502f3832fbecdced50bc7e95cc
Jp Venadas <jaypheevenadas18@gmail.com>
2022-04-16 17:49:23 +0800

    adding Read Functionality

---
+++ addBook.php
@@ -0,0 +1,37 @@
+<?php
+   if(isset($_POST['Add'])){
+     echo $_POST['BookName'];
+     echo $_POST['Author'];
+     echo $_POST['Stocks'];
+   }
+?>
+<button type="button" class=" mb-3 btn btn-primary" data-bs-toggle="modal" data-bs-target="#addBook" data-bs-whatever="@mdo">+ Add Book</button>
+
+<div class="modal fade" id="addBook" tabindex="-1" aria-labelledby="addBookLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="addBookLabel">Add a Book</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action="index.php" method="POST">
+          <div class="mb-3">
+            <input required name="BookName" placeholder="Book Name" type="text" class="form-control" id="BookName">
+          </div>
+          <div class="mb-3">
+            <input required name="Author" placeholder="Author" type="text" class="form-control" id="Author">
+          </div>
+          <div class="mb-3">
+            <input required name="Stocks"  placeholder="Stocks" type="number" class="form-control" id="Stocks">
+          </div>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <input name="Add" type="submit" value="Save" class="btn btn-primary">
+        </form>
+      </div>
+      <div class="modal-footer">
+      </div>
+    </div>
+  </div>
+</div>
+ 
--- index.php
+++ index.php
@@ -1,10 +1,41 @@
 <?php
+$conn = mysqli_connect('127.0.0.2:3307','root','','db_library');
+$command = "select * from TBL_books";
+$res = mysqli_query($conn, $command);
+$books = mysqli_fetch_all($res, MYSQLI_ASSOC);
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
  <?php include('header.php') ?>
- 
+ <div class="container p-4">
+ <?php include('addBook.php')?>
+    <table class="table">
+  <thead class="bg-secondary text-light">
+    <tr>
+    <th scope="col">ID</th>
+      <th scope="col">Book Title</th>
+      <th scope="col">Book Author</th>
+      <th scope="col">Book Stocks</th>
+      <th  scope="col">Operations</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach($books as $book){?>
+    <tr >
+      <td><?php echo $book['BookID']?></td>
+      <td><?php echo $book['BookName']?></td>
+      <td><?php echo $book['Author']?></td>
+      <td><?php echo $book['Stocks']?></td>
+      <td>
+          <Button OnClick=<?php echo "Hello"?> class="btn btn-primary"><i class="bi bi-pen-fill"></i></Button>
+          <Button class="btn btn-danger"><i class="bi bi-trash2-fill"></i></Button>
+    </td>
+    </tr> 
+   <?php } ?>
+  </tbody>
+</table>
+</div>
+ </div>
 </body>
 </html>
