42dd8e6bf205d678a78e527809c3e806aafe04b6
Jp Venadas <jaypheevenadas18@gmail.com>
2022-04-28 13:29:02 +0800

    transaction tabs

--- transactions/transactions.php
+++ transactions/transactions.php
@@ -5,6 +5,19 @@
 <html lang="en">
 
  <?php include('Header.php') ?>
- 
+ <div class="container my-4">
+    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+    <li class="nav-item" role="presentation">
+        <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Borrowed</button>
+    </li>
+    <li class="nav-item" role="presentation">
+        <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Returned</button>
+    </li>
+    </ul>
+    <div class="tab-content" id="pills-tabContent">
+    <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">Hello</div>
+    <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">there</div>
+    </div>
+ </div>
 </body>
 </html>
